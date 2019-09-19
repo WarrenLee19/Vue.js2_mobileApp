48463e73e2649c693a391d83dc2f43765187acf5
WarrenLee <13719507574@163.com>
2019-09-19 23:54:00 +0800

    Add files via upload

---
+++ global-data.js
@@ -0,0 +1,97 @@
+//酒店住宿数据
+let productList=[{
+    src:'../../static/imgs/goods/01.jpg',
+    'pro_name': '标准大床房 [特惠]',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "30m²",//床的大小
+    'pro_cancel': '不可取消',//是否可取消
+    'pro_num': 1,//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 699,//单价
+    'pro_minutes':2,//几分钟前购买了
+    count1:14
+
+},
+{
+    src:'../../static/imgs/goods/05.jpg',
+    'pro_name': '全家旅行房',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "30m²",//床的大小
+    'pro_cancel': '不可取消',//是否可取消
+    'pro_num': '2',//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 300,//单价
+    'pro_minutes':7,
+    count1:1
+},
+{
+    src:"../../static/imgs/goods/02.jpg",
+    'pro_name': '标准商务房',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "20-25m²",//床的大小
+    'pro_cancel': '可取消',//是否可取消
+    'pro_num': 3,//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 90,//单价
+    'pro_minutes':12,
+    count1:2
+},
+{
+    src:"../../static/imgs/goods/03.jpg",'pro_name': '标准商务房',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "20-25m²",//床的大小
+    'pro_cancel': '可取消',//是否可取消
+    'pro_num': 4,//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 180,//单价
+    'pro_minutes':12,
+    count1:12
+},
+{
+    src:"../../static/imgs/goods/04.jpg",'pro_name': '全家旅行房',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "30m²",//床的大小
+    'pro_cancel': '不可取消',//是否可取消
+    'pro_num': 5,//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 220,//单价
+    'pro_minutes':3,
+    count1:15
+},
+{
+    src:"../../static/imgs/goods/01.jpg",'pro_name': '标准商务房',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "20-25m²",//床的大小
+    'pro_cancel': '可取消',//是否可取消
+    'pro_num': 6,//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 300,//单价
+    'pro_minutes':15,
+    count1:30
+},
+{
+    src:"../../static/imgs/goods/01.jpg",'pro_name': '全家旅行房',//床型
+    'pro_btime': '',//入住时间
+    'pro_etime': '',//离店时间
+    'pro_etype': '不含早',//是否含早类型
+    'pro_btype': "30m²",//床的大小
+    'pro_cancel': '不可取消',//是否可取消
+    'pro_num': 7,//数量
+    'pro_img': '../../static/imgs/goods/01.jpg',//图片链接
+    'pro_price': 400,//单价
+    'pro_minutes':3,
+    count1:3
+}];
+
